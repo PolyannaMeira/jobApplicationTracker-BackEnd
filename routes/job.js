2068ff66a83984eb20d3f2bc18701b90a4ac91fb
polyannameira <polyannaxam@gmail.com>
2024-09-22 16:06:50 +0200

    update

--- routes/job.js
+++ routes/job.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/jobs', jobControllers.getAllJobs);
 
 router.get('/job/:id', jobControllers.getJobById);
+router.post('/job', jobControllers.createJobById);
 router.put('/job/:id', jobControllers.updateJobById);
 router.delete('/job/:id', jobControllers.deleteJobById);
 router.get('/jobs/search', jobControllers.searchJobs);
